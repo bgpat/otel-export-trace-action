ed5e1c81096f4dab6ca0bbb61b3a31819bf97c37
Atsushi Tanaka <bgpat@users.noreply.github.com>
2025-01-29 00:46:32 +0900

    Update runner.ts

--- src/runner.ts
+++ src/runner.ts
@@ -36,6 +36,8 @@ export async function run() {
       workflowRunJobs,
     });
     core.setOutput("traceId", spanContext.traceId);
+  } catch(e) {
+    core.error(e);
   } finally {
     core.info("Shutdown Trace Provider");
     setTimeout(() => {
